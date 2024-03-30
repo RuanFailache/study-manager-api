fa2c6a1c2ed8bc717fee7c599b177d1b79fc2aaa
Ruan Failache <ruanfailache@gmail.com>
2024-03-30 14:35:02 -0300

    test: create test for mail service

--- src/main/java/app/studymanager/shared/util/ExceptionUtil.java
+++ src/main/java/app/studymanager/shared/util/ExceptionUtil.java
@@ -4,18 +4,16 @@ import app.studymanager.shared.exception.HttpRequestException;
 import app.studymanager.shared.exception.InternalServerErrorException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionUtil {
-    private static final Logger logger = LoggerFactory.getLogger(ExceptionUtil.class);
-
     public static HttpRequestException handle(Exception exception, String message) {
         if (exception instanceof HttpRequestException) {
             return (HttpRequestException) exception;
         }
-        logger.error(message, exception);
+        log.error(message, exception);
         return new InternalServerErrorException(message);
     }
 }
---
+++ src/test/java/app/studymanager/shared/service/mail/MailServiceTest.java
@@ -0,0 +1,79 @@
+package app.studymanager.shared.service.mail;
+
+import app.studymanager.shared.exception.InternalServerErrorException;
+import app.studymanager.utils.SimulatedException;
+import com.github.javafaker.Faker;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+public class MailServiceTest {
+    @InjectMocks
+    private MailServiceImpl sut;
+
+    @Mock
+    private MailProperties properties;
+
+    @Mock
+    private SendGrid sendGrid;
+
+    private Faker faker;
+
+    @BeforeEach
+    public void setUp() {
+        faker = new Faker();
+    }
+
+    @Test
+    public void testSend() throws IOException {
+        Response expectedResponse = new Response();
+        expectedResponse.setStatusCode(202);
+
+        String to = faker.internet().emailAddress();
+        String subject = faker.lorem().sentence();
+        String text = faker.lorem().paragraph();
+
+        doReturn(expectedResponse).when(sendGrid).api(any(Request.class));
+
+        sut.send(to, subject, text);
+    }
+
+    @Test
+    public void testSendOnInvalidStatusCode() throws IOException {
+        Response expectedResponse = new Response();
+        expectedResponse.setStatusCode(400);
+
+        String to = faker.internet().emailAddress();
+        String subject = faker.lorem().sentence();
+        String text = faker.lorem().paragraph();
+
+        doReturn(expectedResponse).when(sendGrid).api(any(Request.class));
+
+        assertThrows(InternalServerErrorException.class, () -> sut.send(to, subject, text));
+    }
+
+    @Test
+    public void testSendOnSimulatedException() throws IOException {
+        String to = faker.internet().emailAddress();
+        String subject = faker.lorem().sentence();
+        String text = faker.lorem().paragraph();
+
+        doThrow(new SimulatedException()).when(sendGrid).api(any(Request.class));
+
+        assertThrows(InternalServerErrorException.class, () -> sut.send(to, subject, text));
+    }
+}
